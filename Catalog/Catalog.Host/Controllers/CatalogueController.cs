a1e118dfcfd1cab5ad98e44cac38eecd1b0ea747
Herman Orlov <germanriv19@gmail.com>
2023-01-18 15:50:35 +0200

    Added new controller and GET request, nginx

---
+++ Catalog/Catalog.Host/Controllers/CatalogueController.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.Host.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogueController : ControllerBase
+{
+    private static readonly string[] Products = new[] 
+        {"Shorts", "T-shirt","Sweater", "Jacket", "Coat","Jeans","Socks","Tracksuit" };
+
+    private static readonly string[] Colors = new[]
+        {"Green", "Blue","Black", "White", "Red","Violet","Brown","Grey" };
+
+    private readonly ILogger<CatalogueController> _logger;
+
+    public CatalogueController(ILogger<CatalogueController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCatalogue")]
+    public IEnumerable<ClothingPiece> Get()
+    {
+        return Enumerable.Range(1, Products.Length).Select(i => new ClothingPiece()
+        {
+            ObjectsType = Products[Random.Shared.Next(Products.Length)],
+            Color = Colors[Random.Shared.Next(Colors.Length)],
+            Price = Math.Round(Random.Shared.NextDouble() * 100 + 10, 4),
+            CollectionArrivalDate = DateTime.UtcNow.AddDays(Random.Shared.NextDouble() * 10)
+        }).ToArray();
+    }
+}
+
---
+++ Catalog/Catalog.Host/Models/ClothingPiece.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Host.Models
+{
+    public class ClothingPiece
+    {
+        public string ObjectsType { get; set; } = null!;
+        public string Color { get; set; } = null!;
+        public double Price { get; set; }
+        public DateTime CollectionArrivalDate { get; set; }
+    }
+}
